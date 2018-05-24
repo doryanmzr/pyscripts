773046d6c574e64b930cf6f8a990b03863c7595a
doryanmzr <35372228+doryanmzr@users.noreply.github.com>
2018-05-24 18:49:10 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -6,14 +6,14 @@ import smtplib
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.ehlo()
 server.starttls()
-server.login("doryanmzr","doryan0203405")
+server.login("USERNAME","PASSWORD")
 msg1 = "hiii the ping is working"
 msg2 = "hii the ping failed"
 target = "192.168.56.52"
 response = os.system("ping %s -c 3" % target)
 
 if response is not 0:
-	server.sendmail("doryanmzr@gmail.com" , "bryasmin@gmail.com" , msg2)
+	server.sendmail("YYYYY@gmail.com" , "XXXX@gmail.com" , msg2)
 else:
-	server.sendmail("doryanmzr@gmail.com" , "bryasmin@gmail.com" , msg1)
+	server.sendmail("YYYYY@gmail.com" , "XXXX@gmail.com" , msg1)
 
